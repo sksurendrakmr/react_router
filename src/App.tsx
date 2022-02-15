ececd3a7db6483679ede6351f608d0e191978f54
sksurendrakmr <sksurendrakmr@gmail.com>
2022-02-15 23:59:19 +0530

    Active Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
