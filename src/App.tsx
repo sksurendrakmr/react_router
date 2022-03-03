593c73fd428d364a3ed1bcf24c2abcd06e87387b
sksurendrakmr <sksurendrakmr@gmail.com>
2022-03-04 00:06:55 +0530

    Revision

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
