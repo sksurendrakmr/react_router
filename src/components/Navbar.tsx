1cbe38bc62c4547dd161637fe43aaa99487a0478
sksurendrakmr <sksurendrakmr@gmail.com>
2022-03-04 00:46:06 +0530

    5 - Active Links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export const Navbar = () => {
+  /**
+   * M-2 - Using Javascript.
+   * Navlink component will set a property called isActive which can be destructured from
+   * the function parameter.
+   *
+   * isActive will be set to true if the link is indeed the current route.
+   * Thus, we can use this to conditionally apply styles.
+   *
+   * Thus, to set a different style on the active link make use of the Navlink
+   * component. we can wither make use of the active class that is applied or
+   * isActive boolean flag injected into the style function.
+   */
+  const navLinkStyles = ({ isActive }: { isActive: boolean }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink style={navLinkStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 };
