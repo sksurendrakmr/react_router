3813b1fc1847aa254553449122024eb762030208
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-03 23:26:04 +0530

    URL params

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
-  return <div>User Details</div>;
+  //return an object of key value pairs
+  const params = useParams();
+  const userId = params.id; //id corresponds to dynamic segments we config in route.
+  return <div>User Details - {userId}</div>;
 };
